e9106df5626b3143273a5adccf8433c89fb93e2f
Lucas Narciso <lucasnar@gmail.com>
2018-10-21 11:22:59 -0300

    Add type to Internal Transaction tile
    Add a helper function to Internal Transaction view to get the type to be
    displayed. When it is an Internal Transaction of type `:call`, show the
    `:call_type` instead.

--- apps/block_scout_web/lib/block_scout_web/views/internal_transaction_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/internal_transaction_view.ex
@@ -1,3 +1,32 @@
 defmodule BlockScoutWeb.InternalTransactionView do
   use BlockScoutWeb, :view
+
+  alias Explorer.Chain.InternalTransaction
+
+  @doc """
+  Returns the formatted string for the type of the internal transaction.
+
+  When the type is `call`, we return the formatted string for the call type.
+
+  Examples:
+
+  iex> BlockScoutWeb.InternalTransactionView.type(%Explorer.Chain.InternalTransaction{type: :reward})
+  "Reward"
+
+  iex> BlockScoutWeb.InternalTransactionView.type(%Explorer.Chain.InternalTransaction{type: :call, call_type: :delegatecall})
+  "Delegate Call"
+  """
+  def type(%InternalTransaction{type: :call, call_type: call_type}) do
+    formatted_type(call_type)
+  end
+
+  def type(%InternalTransaction{type: type}) do
+    formatted_type(type)
+  end
+
+  defp formatted_type(:call), do: "Call"
+  defp formatted_type(:delegatecall), do: "Delegate Call"
+  defp formatted_type(:create), do: "Create"
+  defp formatted_type(:suicide), do: "Suicide"
+  defp formatted_type(:reward), do: "Reward"
 end
